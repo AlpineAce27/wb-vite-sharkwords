0549e890e5876cebcd1e628a9d68d65cb33a08ed
Kimball Goss <Kimballgoss@gmail.com>
2024-06-24 15:56:50 -0600

    game is working

--- main.js
+++ main.js
@@ -37,6 +37,29 @@ const initSharkwords = () => {
       numWrong += 1
       setSharkImage(document.querySelector('#shark-img'), numWrong)
     }
+    //if all of the letters have been revealed, display you win
+    let isWordComplete = true;
+    for (const el of document.querySelectorAll('.letter-box')) {
+      if (el.innerText === '') {
+        isWordComplete = false;
+        break; // break will exit the loop
+      }
+    }
+    if (isWordComplete === true) {
+      document.querySelector('#game-status').innerText = "You win!!"
+      //console.log("YOU WIN")
+      document.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('disabled', true);
+      })
+    }
+    if (numWrong > 4) {
+      document.querySelector('#game-status').innerText = "You lose!"
+      //console.log("YOU LOSE")
+      document.querySelectorAll('button').forEach((btn) => {
+        btn.setAttribute('disabled', true);
+      })
+    }
+    //if numWrong > 5, disable all the buttons and display you lose
   }
   setupGuesses(document.querySelector('#letter-buttons'), handleGuess)
 }
