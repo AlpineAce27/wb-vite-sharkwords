bce80a50425c0e903098d5534907ea58cfce8a80
Kimball Goss <Kimballgoss@gmail.com>
2024-06-24 15:30:19 -0600

    completed guess handling

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import './style.css';
-import getRandomWord from './src/randomWord.js';
-import setSharkImage from './src/sharkImage.js';
-import {setupWord, isLetterInWord, revealLetterInWord} from './src/word.js';
+import './style.css'
+import getRandomWord from './src/randomWord.js'
+import setSharkImage from './src/sharkImage.js'
+import { setupWord, isLetterInWord, revealLetterInWord } from './src/word.js'
+import setupGuesses from './src/guess.js'
 
 document.querySelector('#app').innerHTML = `
   <section id="shark-img"></section>
@@ -11,18 +12,32 @@ document.querySelector('#app').innerHTML = `
   <section id="word-container"></section>
 
   <section id="letter-buttons"></section>
-`;
+`
+
 
 const initSharkwords = () => {
-  let numWrong = 0;
-  const word = getRandomWord();
+  let numWrong = 0
+  const word = getRandomWord()
   // for debugging:
-  console.log(`[INFO] Correct word is: ${word}`);
+  console.log(`[INFO] Correct word is: ${word}`)
   setSharkImage(document.querySelector('#shark-img'), numWrong)
   setupWord(document.querySelector('#word-container'), word)
   //console.log(isLetterInWord(word[0]))
   //console.log(isLetterInWord('1'))
-  revealLetterInWord(word[0])
-};
+  //revealLetterInWord(word[0])
 
-initSharkwords();
+  const handleGuess = (guessEvent, letter) => {
+    // Disable button after click
+    const button = guessEvent.target
+    button.setAttribute('disabled', true)
+    // Handle correct/incorrect guess
+    if (isLetterInWord(letter)) {
+      revealLetterInWord(letter)
+    } else {
+      numWrong += 1
+      setSharkImage(document.querySelector('#shark-img'), numWrong)
+    }
+  }
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess)
+}
+initSharkwords()
